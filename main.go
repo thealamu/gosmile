e41cc2251158e0b59efefb7ec675b432e2b549f4
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-07 20:23:37 +0100

    Define the container for a joke

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 const jokeEndpoint = "https://official-joke-api.appspot.com/jokes/random"
 
+// A Joke holds info about a joke
+type Joke struct {
+	ID                     int
+	Type, Setup, Punchline string
+}
+
 func main() {
 
 }
