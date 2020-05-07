815a6d3db087115c0ee5000c9fc19a769029d8fa
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-07 20:27:32 +0100

    Get a joke from the endpoint

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 const jokeEndpoint = "https://official-joke-api.appspot.com/jokes/random"
 
 // A Joke holds info about a joke
@@ -9,5 +14,8 @@ type Joke struct {
 }
 
 func main() {
-
+	response, err := http.Get(jokeEndpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
